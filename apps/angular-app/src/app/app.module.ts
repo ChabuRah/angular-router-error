352e298750476effb67bf042acdef96c233ae847
ChabuRah <chaburahade@gmail.com>
2021-12-07 23:11:18 -0600

    add routes

--- apps/angular-app/src/app/app.module.ts
+++ apps/angular-app/src/app/app.module.ts
@@ -1,12 +1,29 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, Injectable, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { AppRoutingModule } from './app-routing,module';
 
 import { AppComponent } from './app.component';
+import { CustomRouteReuseStrategy } from './route0reuse-strategy';
+
+@Injectable()
+class MyErrorHandler implements ErrorHandler {
+  handleError(error: Error) {
+    // do something with the exception
+    alert(error);
+  }
+}
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+		{ provide: RouteReuseStrategy, useClass: CustomRouteReuseStrategy },
+    {
+      provide: ErrorHandler,
+      useClass: MyErrorHandler,
+    },
+	],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
